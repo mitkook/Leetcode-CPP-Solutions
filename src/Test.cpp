06a101aa608e508dcd9666bc06beb9b194ab945d
mitkook <mitkook@gmail.com>
2012-12-17 17:08:04 -0600

    Init Test.cpp

---
+++ src/Test.cpp
@@ -0,0 +1,19 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Dec 17, 2012
+ *      Author: MiTKooK
+ *      E-mail: mitkook (AT) Gmail
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]) {
+
+	cout<<"from \'Interview Questions Online Judge\'"<<endl;
+	cout<<"Test go to http://www.leetcode.com/onlinejudge"<<endl;
+
+	return 0;
+}
